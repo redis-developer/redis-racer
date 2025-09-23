cc5593270cc4d07c24d9f5926d8c69dd977efa0c
Chanh Tran <chanh.tran@redis.com>
2025-09-23 03:44:34 -0500

    minor fixes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@ import app from "./app.js";
 
 const port = process.env.PORT ?? 3000;
 
-console.log(process.env);
-
 app.listen(process.env.PORT ?? 3000, async () => {
   console.log(`game-backend server listening on port ${port}`);
-
 });
